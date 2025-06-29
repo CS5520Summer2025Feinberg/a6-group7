a3c46fd60509aa2864081f0a93d87b63b8a4171d
abhilashsingh <105898930+AbhilashSingh7@users.noreply.github.com>
2025-06-29 08:52:27 -0700

    connected Login to firebase

--- app/src/main/java/edu/northeastern/a6group7/LoginActivity.java
+++ app/src/main/java/edu/northeastern/a6group7/LoginActivity.java
@@ -7,7 +7,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Check SharedPreferences to see if user is already logged in
         SharedPreferences sp = getSharedPreferences("AppData", MODE_PRIVATE);
         String savedUser = sp.getString("username", "");
 
@@ -28,6 +38,7 @@ public class LoginActivity extends AppCompatActivity {
             return;
         }
 
+        // Set login view
         setContentView(R.layout.activity_login);
 
         userInput = findViewById(R.id.usernameInput);
@@ -36,13 +47,26 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String user = userInput.getText().toString();
+                String user = userInput.getText().toString().trim();
 
-                if (user.length() > 0) {
+                if (!user.isEmpty()) {
+                    // Save username locally
                     SharedPreferences.Editor editor = sp.edit();
                     editor.putString("username", user);
-                    editor.commit();
+                    editor.apply();
 
+                    // Save username and data to Firebase RTDB
+                    DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("users");
+                    DatabaseReference currentUserRef = usersRef.child(user);
+
+                    HashMap<String, Object> userData = new HashMap<>();
+                    userData.put("joined", new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault()).format(new Date()));
+                    userData.put("stickersSent", 0);
+                    userData.put("stickersReceived", 0);
+
+                    currentUserRef.setValue(userData);
+
+                    // Navigate to MainActivity
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                     finish();
